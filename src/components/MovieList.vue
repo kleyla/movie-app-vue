4386c36e635fe9ec618fcdffb9df137b55092d46
kleyla <kleyla01@live.com>
2021-01-27 16:44:36 -0400

    first

---
+++ src/components/MovieList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="movie-container">
+    Movie List
+    <Movie v-for="movie in movies" :key="movie.id" :movie="movie" />
+  </div>
+</template>
+
+<script>
+import Movie from "./Movie";
+
+export default {
+  components: {
+    Movie,
+  },
+  data() {
+    return {
+      movies: [
+        { id: 1, name: "movie 1" },
+        { id: 2, name: "movie 2" },
+        { id: 3, name: "movie 3" },
+        { id: 4, name: "movie 4" },
+      ],
+    };
+  },
+  computed: {
+    // movies() {
+    //   return this.$store.getters.getMovies;
+    // },
+  },
+  created() {
+    this.movies = this.$store.getters.getMovies;
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#movie-container {
+  padding-top: 1rem;
+  display: flex;
+  flex-wrap: wrap;
+  flex-grow: 2;
+  justify-content: center;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,29 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import movies from "./modules/movies";
+
+const SET_USER = "SET_USER";
 
 export default createStore({
   state: {
+    users: {},
   },
   mutations: {
+    [SET_USER](state, user) {
+      state.user = user;
+    },
   },
   actions: {
+    setUser({ commit }, user) {
+      // call API
+      commit(SET_USER, user);
+    },
   },
   modules: {
-  }
-})
+    movies,
+  },
+  getters: {
+    isUserAdmin: (state) => {
+      return state.user.role === "admin";
+    },
+  },
+});
